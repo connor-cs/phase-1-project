206639f4be73f7aeb15d64b86b8f36bcc49b93f3
Connor <connor.cs@protonmail.com>
2022-05-12 12:52:41 -0400

    upload

--- app.js
+++ app.js
@@ -8,17 +8,24 @@ const mealName = document.querySelector('#dishName')
 const ingredientName = document.querySelector('#ingredientInput')
 const randomButton = document.querySelector('#random')
 
+function displayMeal(data){
+  console.log(data)
+  console.log(data.strMealThumb)
+  // const randomMealTitle = document.createElement('h1')
+  const randomMeal = document.querySelector('#randomMealContainer')
+  randomMeal.innerHTML = `<h1>Random meal</h1>
+  <img src="${data.meals[0].strMealThumb}" alt="">
+  <p>dfsd</p>
+  <ul>dfdd</ul>
+  <ol>'saddfddf<ol>`
+//   // randomMealTitle.appendChild(randomMeal)
+//   // somevar.c
+  }
 
 
 //random button
 randomButton.addEventListener('click', () => {
-  fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata', {
-    method: 'GET',
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json",
-    }
-  })
+  fetch('https://www.themealdb.com/api/json/v1/1/random.php')
   .then(response => response.json())
-  .then(data => console.log(data.json()))
+  .then(data => displayMeal(data))
 })
